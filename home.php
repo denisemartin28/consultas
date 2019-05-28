1305a5fe14e165529a81ad37b461936a8487e5ef
Denise Martin Caldareri <49910839+denisemartin28@users.noreply.github.com>
2019-05-28 12:16:02 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="stylesheet.css">
+    <title>Growla</title>
+
+  </head>
+  <body>
+    <?php include("header.php") ?>
+    <div id="home">
+    <img src="img\logoblanco.png" alt="logo completo" class="logoblanco">
+    <h1>Somos Growla, una empresa que crece a partir de dos pilares fundamentales: el amor por la birra y la amistad. </h1>
+    </div>
+  </body>
+</html>
